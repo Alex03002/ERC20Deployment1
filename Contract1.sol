67785da9b572efd1eaa779f35ed079a97cf101a0
Alex03002 <158076310+Alex03002@users.noreply.github.com>
2024-11-30 02:32:13 +0800

    Update Contract1.sol

--- Contract1.sol
+++ Contract1.sol
@@ -1,20 +1,32 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.22;
 
-import {ERC20} from "@openzeppelin/contracts@5.1.0/token/ERC20/ERC20.sol";
-import {ERC20Burnable} from "@openzeppelin/contracts@5.1.0/token/ERC20/extensions/ERC20Burnable.sol";
-import {Ownable} from "@openzeppelin/contracts@5.1.0/access/Ownable.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERCToken is ERC20, ERC20Burnable, Ownable {
+contract Token1 is ERC20, Ownable {
     constructor(address initialOwner)
-        ERC20("ERCToken", "ETK")
+        ERC20("Token1", "TK1")
         Ownable(initialOwner)
     {
         _mint(msg.sender, 10000 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
+        require(to != address(0), "Address cannot be 0");
         _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "You cannot burn more than your current balance");
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        require(to != address(0), "Address cannot be 0");
+        require(balanceOf(msg.sender) >= amount, "You cannot transfer more than your current balance");
+        
+        _transfer(msg.sender, to, amount);
+        return true;
     }
 }
